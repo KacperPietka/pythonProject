90f2586230109e8cbc1a2ae51384c395c948f379
Kacper Piętka <103852985+KacperPietka@users.noreply.github.com>
2024-12-31 14:35:08 +0100

    Changes
    Changed Image to flappy changed movement added animations

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import pygame
-from Player import Shiba
+from Player import Flappy
 from Screen import Screen
 from Obstacles import UpperRec
 from Obstacles import LowerRec
@@ -7,10 +7,12 @@ from Obstacles import LowerRec
 class Game:
     def __init__(self):
         self.run = True
-        self.player = Shiba()
+        self.player = Flappy()
         self.screen = Screen(800, 533)
         self.upperRec = UpperRec()
         self.lowerRec = LowerRec()
+        self.last_image_switch_time = 0  # Track time for image switching
+        self.image_index = 0
 
     def game_over(self):
         font = pygame.font.Font('arial.ttf', 32)
@@ -18,31 +20,51 @@ class Game:
         text_rect = text.get_rect()
         self.screen.display.fill((0,0,0))
 
+    def update_flappy_image(self):
+        current_time = pygame.time.get_ticks()
+        if current_time - self.last_image_switch_time >= 300:
+            self.last_image_switch_time = current_time
+            self.image_index = (self.image_index + 1) % 3
+
+    def display_flappy_image(self):
+        if self.image_index == 0:
+            self.player.display_image_1(self.screen.display)
+        elif self.image_index == 1:
+            self.player.display_image_2(self.screen.display)
+        elif self.image_index == 2:
+            self.player.display_image_3(self.screen.display)
+
+    def update_obstacles_position(self):
+        self.upperRec.position[0] -= 2
+        self.lowerRec.position[0] -= 2
+    def draw_obstacles(self):
+        pygame.draw.rect(self.screen.display, (0, 123, 0),
+                         pygame.Rect(self.upperRec.position[0], self.upperRec.position[1], self.upperRec.size[0],
+                                     self.upperRec.size[1]))
+        pygame.draw.rect(self.screen.display, (0, 123, 0),
+                         pygame.Rect(self.lowerRec.position[0], self.lowerRec.position[1], self.lowerRec.size[0],
+                                     self.lowerRec.size[1]))
     def game_loop(self):
         run = True
         clock = pygame.time.Clock()
         while run:
+            time_counter = clock.tick()
             clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
             key = pygame.key.get_pressed()
-            pygame.draw.rect(self.screen.display, (0, 123, 0), pygame.Rect(30, 30, 60, 60), 2)
             self.player.update_position(key)
             self.player.position[1] += 1
-            self.upperRec.position[0] -= 2
-            self.lowerRec.position[0] -= 2
+            self.update_obstacles_position()
             if self.player.position[1] >= self.screen.height - self.player.height or self.player.position[1] <= 0:
                 #self.game_over()
                 run = False
             self.screen.display.fill((0, 0, 128))
-            pygame.draw.rect(self.screen.display, (0, 123, 0),
-                             pygame.Rect(self.upperRec.position[0], self.upperRec.position[1], self.upperRec.size[0],
-                                         self.upperRec.size[1]))
-            pygame.draw.rect(self.screen.display, (0, 123, 0),
-                             pygame.Rect(self.lowerRec.position[0], self.lowerRec.position[1], self.lowerRec.size[0],
-                                         self.lowerRec.size[1]))
-            self.player.display(self.screen.display)
+            self.draw_obstacles()
+
+            self.update_flappy_image()
+            self.display_flappy_image()
 
             pygame.display.update()
         pygame.quit()
--- Player.py
+++ Player.py
@@ -2,14 +2,20 @@ import pygame
 from Screen import Screen
 
 
-class Shiba:
+class Flappy:
     def __init__(self):
-        self.image = pygame.image.load('shiba_sprite.png')
+        self.image1 = pygame.image.load('flappy.png')
+        self.image2 = pygame.image.load('flappy2.png')
+        self.image3 = pygame.image.load('flappy3.png')
         self.position = [10, 200]
         self.height = 84
         self.width = 114
     def update_position(self, key):
         if key[pygame.K_SPACE]:
             self.position[1] -= 10
-    def display(self, screen):
-        screen.blit(self.image, (self.position[0], self.position[1]))
+    def display_image_1(self, screen):
+        screen.blit(self.image1, (self.position[0], self.position[1]))
+    def display_image_2(self, screen):
+        screen.blit(self.image2, (self.position[0], self.position[1]))
+    def display_image_3(self, screen):
+        screen.blit(self.image3, (self.position[0], self.position[1]))
