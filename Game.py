a084d1fb29f843c0d3457424da6a095a3a818ab2
Kacper Piętka <103852985+KacperPietka@users.noreply.github.com>
2024-12-31 11:10:04 +0100

    Upper rectangle
    Added upper rectangle and movement

--- Game.py
+++ Game.py
@@ -1,21 +1,22 @@
 import pygame
 from Player import Shiba
 from Screen import Screen
-from Obstacles import Rectangles
-
-pygame.init()
-player = Shiba()
-screen = Screen(800, 533)
-rectangle = Rectangles()
+from Obstacles import UpperRec
+from Obstacles import LowerRec
 
 class Game:
     def __init__(self):
         self.run = True
+        self.player = Shiba()
+        self.screen = Screen(800, 533)
+        self.upperRec = UpperRec()
+        self.lowerRec = LowerRec()
 
     def game_over(self):
         font = pygame.font.Font('arial.ttf', 32)
         text = font.render('YOU LOST!', True, (0, 0, 0))
         text_rect = text.get_rect()
+        self.screen.display.fill((0,0,0))
 
     def game_loop(self):
         run = True
@@ -26,18 +27,22 @@ class Game:
                 if event.type == pygame.QUIT:
                     run = False
             key = pygame.key.get_pressed()
-            pygame.draw.rect(screen.display, (0, 123, 0), pygame.Rect(30, 30, 60, 60), 2)
-            player.update_position(key)
-            player.position[1] += 2
-            rectangle.position[0] -= 2
-            if player.position[1] >= screen.height - player.height or player.position[1] <= 0:
-                self.game_over()
+            pygame.draw.rect(self.screen.display, (0, 123, 0), pygame.Rect(30, 30, 60, 60), 2)
+            self.player.update_position(key)
+            self.player.position[1] += 1
+            self.upperRec.position[0] -= 2
+            self.lowerRec.position[0] -= 2
+            if self.player.position[1] >= self.screen.height - self.player.height or self.player.position[1] <= 0:
+                #self.game_over()
                 run = False
-            screen.display.fill((0, 0, 128))
-            pygame.draw.rect(screen.display, (0, 123, 0),
-                             pygame.Rect(rectangle.position[0], rectangle.position[1], rectangle.size[0],
-                                         rectangle.size[1]))
-            player.display(screen.display)
+            self.screen.display.fill((0, 0, 128))
+            pygame.draw.rect(self.screen.display, (0, 123, 0),
+                             pygame.Rect(self.upperRec.position[0], self.upperRec.position[1], self.upperRec.size[0],
+                                         self.upperRec.size[1]))
+            pygame.draw.rect(self.screen.display, (0, 123, 0),
+                             pygame.Rect(self.lowerRec.position[0], self.lowerRec.position[1], self.lowerRec.size[0],
+                                         self.lowerRec.size[1]))
+            self.player.display(self.screen.display)
 
             pygame.display.update()
         pygame.quit()
--- Obstacles.py
+++ Obstacles.py
@@ -1,4 +1,10 @@
-class Rectangles:
+class LowerRec:
     def __init__(self):
-        self.position = [650, 250]
+        self.position = [750, 250]
         self.size = [60, 800]
+    def spawn_rec(self):
+        self.position = [750, 250]
+class UpperRec:
+    def __init__(self):
+        self.position = [750, 0]
+        self.size = [60, 100]
